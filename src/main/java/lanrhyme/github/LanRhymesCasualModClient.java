b2a246344f9c90fc15da7414b917e94e30d99b4b
LanRhyme <xiao_ren233@foxmail.com>
2024-06-21 20:48:57 +0800

    1

---
+++ src/main/java/lanrhyme/github/LanRhymesCasualModClient.java
@@ -0,0 +1,10 @@
+package lanrhyme.github;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LanRhymesCasualModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
